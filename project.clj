1a2610806082af0a7e2cf5b67d0393b904a3c9c1
jsa-aerial <jsa.aerial@gmail.com>
2018-07-30 17:06:58 -0400

    CSV useful in examples

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async "0.4.474"]
+                 [org.clojure/data.csv "0.1.3"]
 
                  [ring/ring-defaults "0.3.1"]
                  [bk/ring-gzip "0.2.1"]

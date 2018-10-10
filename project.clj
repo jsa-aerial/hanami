4a3bf7061502f5a00fcb8b02d943c01c75d94232
jsa-aerial <jsa.aerial@gmail.com>
2018-10-10 11:58:03 -0400

    Bump version for new work

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.hanami "0.2.0"
+(defproject aerial.hanami "0.2.1"
   :description "Clojure(Script) client/server vega-lite-2 based dynamic interactive plots and charts"
   :url "https://github.com/jsa-aerial/hanami"
   :license {:name "The MIT License (MIT)"
@@ -25,6 +25,11 @@
                  [cljsjs/vega-lite "2.6.0-0"]
                  [cljsjs/vega-embed "3.16.1-0"]
                  [cljsjs/vega-tooltip "0.12.0-0"]
+
+                 ;; [cljsjs/vega "4.2.0-0"]
+                 ;; [cljsjs/vega-lite "3.0.0-rc6-0"]
+                 ;; [cljsjs/vega-embed "3.20.0-0"]
+                 ;; [cljsjs/vega-tooltip "0.13.0-0"]
                  ]
 
   :plugins [[lein-figwheel "0.5.16"]
@@ -32,7 +37,7 @@
 
   :source-paths ["src/cljc" "src/clj" "src/cljs"]
 
-  ;;:prep-tasks ["compile" ["cljsbuild" "once" "min"]]
+  ;:prep-tasks ["compile" ["cljsbuild" "once" "min"]]
 
   :cljsbuild {:builds
               [{:id "dev"

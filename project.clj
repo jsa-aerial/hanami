aa365a0a526ea3e4bf91839bca45938a91744c54
jsa-aerial <jsa.aerial@gmail.com>
2019-09-17 12:16:48 -0400

    Bump version to latest changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.hanami "0.10.3"
+(defproject aerial.hanami "0.10.5"
   :description "Clojure(Script) client/server vega-lite-2 based dynamic interactive plots and charts"
   :url "https://github.com/jsa-aerial/hanami"
   :license {:name "The MIT License (MIT)"

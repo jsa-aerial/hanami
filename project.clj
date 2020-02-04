b091a788c78f1d2b42970819fcc9833aca0e2a48
jsa-aerial <jsa.aerial@gmail.com>
2020-02-04 14:01:06 -0500

    new version for added vspec/vgview capability

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.hanami "0.10.13"
+(defproject aerial.hanami "0.11.0"
   :description "Clojure(Script) client/server vega/vega-lite based dynamic interactive plots and charts"
   :url "https://github.com/jsa-aerial/hanami"
   :license {:name "The MIT License (MIT)"
